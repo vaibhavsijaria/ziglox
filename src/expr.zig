8437f0572fae741eee27dcd40c75a3cca15a738d
Vaibhav Sijaria <139199971+vaibhavsijaria@users.noreply.github.com.>
2024-07-17 22:51:50 +0530

    implementing expressions for parser

---
+++ src/expr.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const Tokens = @import("tokens.zig");
+
+const Token = Tokens.Token;
+const obj = Tokens.obj;
+
+pub const Expr = union(enum) {
+    Binary: *Binary,
+    Grouping: *Grouping,
+    Literal: *Literal,
+    Unary: *Unary,
+
+    pub fn accept(self: Expr, visitor: anytype) @TypeOf(visitor).ReturnType {
+        return switch (self) {
+            .Binary => |b| visitor.visitBinaryExpr(b),
+            .Grouping => |g| visitor.visitGroupingExpr(g),
+            .Literal => |l| visitor.visitLiteralExpr(l),
+            .Unary => |u| visitor.visitUnaryExpr(u),
+        };
+    }
+};
+
+pub const Binary = struct {
+    left: *Expr,
+    operator: Token,
+    right: *Expr,
+};
+
+pub const Grouping = struct {
+    expression: *Expr,
+};
+
+pub const Literal = struct {
+    value: ?obj,
+};
+
+pub const Unary = struct {
+    operator: Token,
+    right: *Expr,
+};
